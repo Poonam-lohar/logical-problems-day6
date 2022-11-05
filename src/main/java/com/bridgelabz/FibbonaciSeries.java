64a5cd5a8158e84f2688eb9cb29a26b42d73230a
Your Name <you@example.com>
2022-11-05 15:34:01 +0530

    Added:Logical problems

---
+++ src/main/java/com/bridgelabz/FibbonaciSeries.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class FibbonaciSeries {
+
+    public static void main(String[] args) {
+
+        int n, first = 0, next = 1;
+        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        System.out.print("The first " + n + " Fibonacci numbers are: ");
+        System.out.println(first + " " + next);
+        for (
+                int i = 1;
+                i <= n - 2; ++i) {
+            int sum = first + next;
+            first = next;
+            next = sum;
+            System.out.println(" " + sum);
+        }
+    }
+}
---
+++ src/main/java/com/bridgelabz/stopwatch.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class stopwatch {
+    public static void main(String[] args){
+        long startTime=System.currentTimeMillis();
+        for (int i =0; i<10; i++) {
+            int random = (int) Math.floor(Math.random() * 10);
+            System.out.println(random);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Elapsed time is: " + (endTime - startTime));
+
+    }
+}
